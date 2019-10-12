6522165285717556d894b84df4c8f9e518d32e1c
Guilherme Leonhardt Santa Maria <guileonhardt2@gmail.com>
2019-10-11 23:12:51 -0300

    Lista de post e post criados

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="titulo">
-    <span></span>
+    <!-- <span>{{post.titulo}}</span> -->
+    <span>post</span>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {}
+};
+</script>
---
+++ src/components/PostList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="listaPost">
+    <span>a</span>
+    <ul v-if="posts.length > 0">
+      <Post v-for="post in posts" :key="post.id" :post="post" />
+    </ul>
+    <span v-else>Nenhum post encontrado</span>
+  </div>
+</template>
+
+<script>
+import api from "../api";
+import Post from "./Post";
+
+export default {
+  name: "PostList",
+  components: {
+    Post
+  },
+  data() {
+    return {
+      posts: []
+    };
+  },
+  computed: {},
+  created() {
+    api.get(api.defaults.backendUrl + "post").then(response => {
+      this.posts = response.data;
+      console.log(this.posts);
+    });
+  }
+};
+</script>
+
+<style lang="scss" >
+.listaPost {
+  height: 100%;
+  background-color: #e5e5e5;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
